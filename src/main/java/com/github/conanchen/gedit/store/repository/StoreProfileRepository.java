cf44e10ac099cce9dbd32028afa2e60e92f2a131
conanchen <conan8chan@yahoo.com>
2018-01-12 18:37:17 +0800

    Revert "specified type"
    This reverts commit d2ee722

--- src/main/java/com/github/conanchen/gedit/store/repository/StoreProfileRepository.java
+++ src/main/java/com/github/conanchen/gedit/store/repository/StoreProfileRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.io.Serializable;
 import java.util.List;
 
-public interface StoreProfileRepository<StoreProfile,String extends Serializable> extends JpaRepository<StoreProfile,String> {
+public interface StoreProfileRepository<T,String extends Serializable> extends JpaRepository<StoreProfile,String> {
     StoreProfile findByName(String name);
 
     List<StoreProfile> findByType(String type, Pageable request);
