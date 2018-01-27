113d3cc0a0c21f908aef17cd39140081b9931394
hai <hilin2333@gmail.com>
2018-01-27 17:31:04 +0800

    bug fix

--- src/main/java/com/github/conanchen/gedit/store/grpc/client/SearchClient.java
+++ src/main/java/com/github/conanchen/gedit/store/grpc/client/SearchClient.java
@@ -3,14 +3,17 @@ package com.github.conanchen.gedit.store.grpc.client;
 import com.github.conanchen.gedit.common.grpc.Location;
 import com.github.conanchen.gedit.store.model.StoreProfile;
 import com.github.conanchen.gedit.store.search.grpc.IndexStoreRequest;
+import com.github.conanchen.gedit.store.search.grpc.IndexStoreResponse;
 import com.github.conanchen.gedit.store.search.grpc.StoreSearchApiGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-
+@Slf4j
 @Component
 public class SearchClient {
     private static final String EMPTY_STR = "";
@@ -19,17 +22,17 @@ public class SearchClient {
     //channel
     private ManagedChannel channel;
     // search stub
-    private StoreSearchApiGrpc.StoreSearchApiBlockingStub searchApiBlockingStub;
+    private StoreSearchApiGrpc.StoreSearchApiStub searchApiStub;
     @PostConstruct
     public void init() {
         channel = ManagedChannelBuilder.forAddress(uri,9985)
                 .usePlaintext(true)
                 .build();
-        searchApiBlockingStub = StoreSearchApiGrpc.newBlockingStub(channel);
+        searchApiStub = StoreSearchApiGrpc.newStub(channel);
     }
 
     public void index(StoreProfile storeProfile){
-        searchApiBlockingStub.index(IndexStoreRequest.newBuilder()
+        searchApiStub.index(IndexStoreRequest.newBuilder()
                 .setDesc(storeProfile.getDescr() == null ? EMPTY_STR : storeProfile.getDescr())
                 .setPointsRate(storeProfile.getPointsRate() == null ? 0.0D : storeProfile.getPointsRate())
                 .setLocation(Location.newBuilder()
@@ -40,6 +43,25 @@ public class SearchClient {
                 .setName(storeProfile.getName())
                 .setType(storeProfile.getType() == null ? "" : storeProfile.getType())
                 .setUuid(storeProfile.getUuid())
-                .build());
+                .build(),
+                new StreamObserver<IndexStoreResponse>(){
+
+                    @Override
+                    public void onNext(IndexStoreResponse value) {
+                        log.info("success");
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        log.info("error");
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        log.info("complete");
+                    }
+                }
+
+        );
     }
 }
